ed01e1698c51e5a0ff9987327932fd55bed7654b
kk2115 <80417519+kk2115@users.noreply.github.com>
2023-05-04 20:36:13 +0200

    algorytm dziala

--- Hungarian_Algorithm.py
+++ Hungarian_Algorithm.py
@@ -12,13 +12,52 @@ class AlgorytmWegierski:
         self.listaKolumnZeZaleznych = []
         self.listaZerNiezaleznych = {} # lista przechowuje zera niezalezne w formacie listaZerNiezaleznych[kolumna] = wiersz
 
-
         self.suma_redukcji_phi = 0
-        self.listaWykreslen = [[],  #pierwszy element jest listą rzedow z zerami niezaleznymi
-                               []]  #drugi lista kolumn z zerami niezaleznymi
 
         self.wykresloneWiersze =[]
         self.wykresloneKolumny = []
+
+
+    def matrix_redux(self):
+
+        for row in range(len(self.macierz)):
+            #zredukuj wiersze
+            min_element_row = min(self.macierz[row])
+            self.macierz[row] = self.macierz[row] - min_element_row
+            self.suma_redukcji_phi += min_element_row
+
+        #zredukuj kolumny
+        for col in range(len(self.macierz)):
+            min_element_col = min(self.macierz[:, col])
+            self.macierz[:, col] = self.macierz[:, col] - min_element_col
+            self.suma_redukcji_phi += min_element_col
+
+
+    def znajdzZeraNiezalezne(self):
+        #zanjdz zera w tablicy
+        for zeroRowIndex, matrixRow in enumerate(self.macierz):
+            for zeroColIndex, matrixElement in enumerate(matrixRow):
+                if matrixElement == 0:
+                    # ozanacz jako zalezne albo niezalezne
+                    if self.sprawdzCzyNiezalezne(zeroRowIndex, zeroColIndex):
+                        self.listaKolumnZerNiezaleznych.append(zeroColIndex)
+                        self.listaWierszyZerNiezaleznych.append(zeroRowIndex)
+                        self.listaZerNiezaleznych[zeroColIndex] = zeroRowIndex
+                    else:
+                        self.listaKolumnZeZaleznych.append(zeroColIndex)
+
+
+    def sprawdzCzyNiezalezne(self, elementRow, elementCol):
+        #sprawdz czy w wierszu jest juz zero
+        if elementRow in self.listaWierszyZerNiezaleznych:
+            return False
+
+        #sprawdz czy w kolumnie jest juz zero
+        if elementCol in self.listaKolumnZerNiezaleznych:
+            return False
+
+        return True
+
 
 
     def wykreslLinie(self):
@@ -48,47 +87,60 @@ class AlgorytmWegierski:
         self.wykresloneKolumny = listaOznakowanychKolumn
 
 
-    def matrix_redux(self):
+    def powiekszZbiorZerNiezaleznych(self):
+        # tworzymy liste elementow macierzy bez elementow wykreslonych
+        filtered_list = [self.macierz[i][j] for i in range(len(self.macierz)) for j in range(len(self.macierz[i])) \
+                         if i not in self.wykresloneWiersze and j not in self.wykresloneKolumny]
 
-        for row in range(len(self.macierz)):
-            #zredukuj wiersze
-            min_element_row = min(self.macierz[row])
-            self.macierz[row] = self.macierz[row] - min_element_row
-            self.suma_redukcji_phi += min_element_row
+        # znajdujemy najmniejszą wartość
+        min_value = min(filtered_list)
 
-        #zredukuj kolumny
-        for col in range(len(self.macierz)):
-            min_element_col = min(self.macierz[:, col])
-            self.macierz[:, col] = self.macierz[:, col] - min_element_col
-            self.suma_redukcji_phi += min_element_col
+        for rowIndex in range(len(self.macierz)):
+            for colIndex in range(len(self.macierz[rowIndex])):
+                if colIndex not in self.wykresloneKolumny and  rowIndex not in self.wykresloneWiersze:
+                    self.macierz[rowIndex][colIndex] -= min_value
+                elif colIndex in self.wykresloneKolumny and rowIndex in self.wykresloneWiersze:
+                    self.macierz[rowIndex][colIndex] += min_value
 
-
-    def sprawdzCzyNiezalezne(self, elementRow, elementCol):
-        #sprawdz czy w wierszu jest juz zero
-        if elementRow in self.listaWierszyZerNiezaleznych:
-            return False
-
-        #sprawdz czy w kolumnie jest juz zero
-        if elementCol in self.listaKolumnZerNiezaleznych:
-            return False
-
-        return True
+        self.suma_redukcji_phi += min_value
 
 
-    def znajdzZeraNiezalezne(self):
-        #zanjdz zera w tablicy
-        for zeroRowIndex, matrixRow in enumerate(self.macierz):
-            for zeroColIndex, matrixElement in enumerate(matrixRow):
-                if matrixElement == 0:
-                    # ozanacz jako zalezne albo niezalezne
-                    if self.sprawdzCzyNiezalezne(zeroRowIndex, zeroColIndex):
-                        self.listaKolumnZerNiezaleznych.append(zeroColIndex)
-                        self.listaWierszyZerNiezaleznych.append(zeroRowIndex)
-                        self.listaZerNiezaleznych[zeroColIndex] = zeroRowIndex
-                    else:
-                        self.listaKolumnZeZaleznych.append(zeroColIndex)
+    def wyznaczMacierzRozwiazania(self):
+        rozmiar = len(self.macierz)
+        rozwiazanie = np.zeros((rozmiar, rozmiar))
+
+        for col in self.listaZerNiezaleznych:
+            row = self.listaZerNiezaleznych[col]
+            rozwiazanie[row][col] = 1
+
+        return rozwiazanie
 
 
+    def rozwiazanieAlgorytmu(self):
+
+        while True:
+            # krok 1. zredukuj macierz:
+            self.matrix_redux()
+
+            # krok 2. znajdz zera niezalezne:
+            self.znajdzZeraNiezalezne()
+
+            # sprawdz czy znaleziono rozwiazanie
+            if len(self.listaZerNiezaleznych) == len(self.macierz):
+                break
+
+            # krok 3. wykreslanie wierszy i kolumn
+            self.wykreslLinie()
+
+            # sprawdz czy znaleziono rozwiazanie
+            if len(self.wykresloneKolumny) + len(self.wykresloneWiersze) == len(self.macierz):
+                break
+
+            # krok 4. powieksz zbior zer niezaleznych
+            self.powiekszZbiorZerNiezaleznych()
+
+        print("wartosc funkcji kryterialnej: ", self.suma_redukcji_phi)
+        print(self.wyznaczMacierzRozwiazania())
 
 def main():
     A = np.array([[5, 2, 3, 2, 7],
@@ -97,20 +149,8 @@ def main():
                   [6, 9, 0, 4, 0],
                   [4, 1, 2, 4, 0]])
 
-
     Test = AlgorytmWegierski(A)
-    Test.matrix_redux()
-    Test.znajdzZeraNiezalezne()
-    Test.wykreslLinie()
-
-
-    print(Test.wykresloneWiersze)
-    print(Test.wykresloneKolumny)
-
-    # print(Test.listaKolumnZerNiezaleznych)
-    # print(Test.listaWierszyZerNiezaleznych)
-    # print(Test.listaZerNiezaleznych)
-    # print(Test.macierz)
+    Test.rozwiazanieAlgorytmu()
 
 
 if __name__ == '__main__':
